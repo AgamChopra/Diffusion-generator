22a94846c1fb4e28a03082b080d360943382c854
Agam Chopra <achopra4@uw.edu>
2022-08-24 16:25:10 -0400

    initial code added

---
+++ diffusion_models.py
@@ -0,0 +1,123 @@
+import torch
+import torch.nn as nn
+
+
+def pad2d(input_, target, device = 'cpu'):
+    output_ = torch.zeros((target.shape[0],input_.shape[1],target.shape[2],target.shape[3]),device=device)
+    start_idx = int((output_.shape[-1] - input_.shape[-1]) / 2)
+    try:
+        output_[:,:,start_idx:-start_idx,start_idx:-start_idx] = input_
+    except:
+        try:
+            output_[:,:,start_idx:-start_idx-1,start_idx:-start_idx-1] = input_
+        except:
+            output_[:,:,1:,1:] = input_
+    return output_
+
+
+class Block(nn.Module):
+    def __init__(self,in_c,embd_dim,out_c,hid_c=None):
+        super(Block, self).__init__()
+        
+        if hid_c is None:
+            self.mlp = nn.Linear(embd_dim,out_c)
+            
+            self.layer = nn.Sequential(nn.Conv2d(in_channels = in_c, out_channels = out_c, kernel_size = 3),nn.ReLU(),nn.BatchNorm2d(out_c))
+            
+            self.out_block = nn.Sequential(nn.Conv2d(in_channels = out_c, out_channels = out_c, kernel_size = 3),nn.ReLU(),nn.BatchNorm2d(out_c))
+        else:
+            self.mlp = nn.Linear(embd_dim,hid_c)
+            
+            self.layer = nn.Sequential(nn.Conv2d(in_channels = in_c, out_channels = hid_c, kernel_size = 3),nn.ReLU(),nn.BatchNorm2d(hid_c))
+            
+            self.out_block = nn.Sequential(nn.Conv2d(in_channels = hid_c, out_channels = hid_c, kernel_size = 3),nn.ReLU(),nn.BatchNorm2d(hid_c),
+                                           nn.ConvTranspose2d(in_channels = hid_c, out_channels = out_c, kernel_size = 2, stride= 2),nn.ReLU(),nn.BatchNorm2d(out_c))
+        
+    def forward(self,x,t):
+        t = self.mlp(t)
+        y = self.layer(x)
+        t = t[(..., ) + (None, ) * 2]
+        y = y + t
+        y = self.out_block(y)
+        return y
+
+
+class UNet(nn.Module):
+    def __init__(self,CH=3,t_emb=32):
+        super(UNet, self).__init__()
+        n = 1
+        #layers
+        self.layer1 = Block(in_c = CH, embd_dim = t_emb, out_c = int(64/n))
+        
+        self.layer2 = Block(in_c = int(64/n), embd_dim = t_emb, out_c = int(128/n))
+        
+        self.layer3 = Block(in_c = int(128/n), embd_dim = t_emb, out_c = int(256/n))
+        
+        self.layer4 = Block(in_c = int(256/n), embd_dim = t_emb, out_c = int(512/n))
+        
+        self.layer5 = Block(in_c = int(512/n), embd_dim = t_emb, out_c = int(512/n), hid_c = int(1024/n))
+        
+        self.layer6 = Block(in_c = int(1024/n), embd_dim = t_emb, out_c = int(256/n), hid_c = int(512/n))
+        
+        self.layer7 = Block(in_c = int(512/n), embd_dim = t_emb, out_c = int(128/n), hid_c = int(256/n))
+        
+        self.layer8 = Block(in_c = int(256/n), embd_dim = t_emb, out_c = int(64/n), hid_c = int(128/n))
+        
+        self.layer9 = Block(in_c = int(128/n), embd_dim = t_emb, out_c = int(64/n))
+        
+        self.out = nn.Conv2d(in_channels = int(64/n), out_channels = CH, kernel_size = 1)
+        
+        self.maxpool = nn.MaxPool2d(kernel_size=2,stride=2)
+        
+        
+    def forward(self,x,t,device = 'cuda:0'):
+        
+        y1 = self.layer1(x,t)
+        y = self.maxpool(y1)
+        
+        y2 = self.layer2(y,t)
+        y = self.maxpool(y2)
+        
+        y3 = self.layer3(y,t)
+        y = self.maxpool(y3)
+        
+        y4 = self.layer4(y,t)
+        y = self.maxpool(y4)
+        
+        y = self.layer5(y,t)
+        
+        y = torch.cat((y4,pad2d(y, y4, device = device)),dim = 1)
+        y = self.layer6(y,t)
+        
+        y = torch.cat((y3,pad2d(y, y3, device = device)),dim = 1)
+        y = self.layer7(y,t)
+        
+        y = torch.cat((y2,pad2d(y, y2, device = device)),dim = 1)
+        y = self.layer8(y,t)
+        
+        y = torch.cat((y1,pad2d(y, y1, device = device)),dim = 1)
+        y = self.layer9(y,t)
+        
+        y = pad2d(y, x, device = device)
+        
+        y = self.out(y)
+        
+        return y
+    
+
+def test(device = 'cpu'):
+    batch = 32
+    a = torch.ones((batch,3,140,140),device=device)
+    t = torch.ones((batch,32),device=device)
+    
+    model = UNet().to(device)
+    print(model)
+    
+    b = model(a,t,device)
+    
+    print(a.shape,t.shape)
+    print(b.shape)
+        
+        
+if __name__ == '__main__':
+    test('cpu')
---
+++ diffusion_train.py
@@ -0,0 +1,178 @@
+import torch
+from tqdm import trange
+import random
+
+import diffusion_models as models
+import dataset as dst
+
+
+print('cuda detected:',torch.cuda.is_available())
+
+
+def getPositionEncoding(seq_len, d=512, n=10000):
+    P = torch.zeros((seq_len, d))
+    for k in range(seq_len):
+        for i in torch.arange(int(d/2)):
+            denominator = torch.pow(n, 2*i/d)
+            P[k, 2*i] = torch.sin(k/denominator)
+            P[k, 2*i+1] = torch.cos(k/denominator)
+    return P
+
+
+def norm(x):
+    return (x-torch.min(x))/(torch.max(x)-torch.min(x))
+
+
+def apply_random_noise(x,a1,a2,t_encodings):
+    x_epsilon = torch.rand_like(x)
+    list_len = len(a1)
+    idx = torch.tensor(random.sample(range(1, list_len), x.shape[0]))
+    
+    x_t1 = a1[idx-1].view(len(idx),1,1,1) * x + a2[idx-1].view(len(idx),1,1,1) * x_epsilon
+    x_t2 = a1[idx].view(len(idx),1,1,1) * x + a2[idx].view(len(idx),1,1,1) * x_epsilon
+    t2 = t_encodings[idx]
+    
+    return x_t1, x_t2, t2
+
+
+class noise_generator():
+    def __init__(self, T = 1000, b_0 = 0.0001, b_t = 0.02, time_encoding_dim = 128):
+        alpha_bar = torch.cumprod(1. - torch.linspace(b_0,b_t,T), dim=0)
+        self.a1 = torch.cat([torch.ones(1), alpha_bar ** 0.5, torch.zeros(1)])
+        self.a2 = torch.cat([torch.zeros(1), (1 - alpha_bar) ** 0.5, torch.ones(1)])
+        self.t_encode = getPositionEncoding(T,time_encoding_dim) 
+        
+    def apply(self, x0):
+        return apply_random_noise(x0,self.a1,self.a2,self.t_encode)
+        
+
+def diff_train(dataset, lr = 1E-4, epochs = 5, batch=32, beta1=0.5, beta2=0.999, T=1000, time_encoding_dim = 32, dmt = 32, load_state = False, state=None):   
+    REG = int(batch / dmt)
+    batch -=  REG 
+    
+    if T > 10:
+        ctr_loops = int(T/10)
+    else:
+        ctr_loops = 1
+        
+    print(T,ctr_loops)
+    
+    Glosses = []
+    
+    CH = dataset.shape[1]
+    
+    print('loading generator...', end =" ")
+    #Generator 
+    Gen = models.UNet(CH).cuda()
+    
+    if load_state:
+        print('loading previous run state...', end =" ")
+        Gen.load_state_dict(torch.load("E:\ML\Dog-Cat-GANs\Gen-diff-Autosave.pt")) 
+        print('done.')
+        
+    if state is not None:
+        Gen.load_state_dict(torch.load(state)) 
+    
+    optimizerG = torch.optim.Adam(Gen.parameters(),lr,betas=(beta1, beta2))
+    
+    print('loading noise generator...')
+    noise = noise_generator(T=T, time_encoding_dim=time_encoding_dim)
+    
+    print('loading error function...')
+    error = torch.nn.MSELoss()
+    
+    print('optimizing...')
+    
+    for eps in trange(epochs):        
+        idx_ = torch.randperm(dataset.shape[0])
+        ctr = 1
+        
+        while ctr <= ctr_loops:          
+            for b in range(0,dataset.shape[0]-batch,batch):  
+                
+                optimizerG.zero_grad()       
+                
+                xt1,xt2,t2 = noise.apply(norm(dataset[idx_[b:b+batch]]))
+                xt1,xt2,t2 = xt1.cuda(), xt2.cuda(), t2.cuda()
+                
+                xt1p = Gen(xt2,t2)
+                
+                errG = error(xt1,xt1p)
+                errG.backward()
+                optimizerG.step()
+    
+                Glosses.append(errG.item())
+                    
+                if ctr % ctr_loops/2 == 0:
+                    print('[%d/%d][%d/%d]\tLoss: %.4f'% (eps, epochs, int(b/batch), int(len(idx_)/batch), errG.item()))  
+                    print(torch.max(xt1),torch.min(xt1),torch.mean(xt1),torch.var(xt1))
+                    print(torch.max(xt2),torch.min(xt2),torch.mean(xt2),torch.var(xt2))
+                    print(torch.max(xt1p),torch.min(xt1p),torch.mean(xt1p),torch.var(xt1p))
+                    dst.visualize(norm(xt1[0]).cpu().detach().numpy(),dark = False, title='X_t1')
+                    dst.visualize(norm(xt2[0]).cpu().detach().numpy(),dark = False, title='X_t2')
+                    dst.visualize(norm(xt1p[0]).cpu().detach().numpy(),dark = True, title='X_t2 -> X_t1')
+                    torch.save(Gen.state_dict(), "E:\ML\Dog-Cat-GANs\Gen-diff-Autosave.pt")
+                        
+                ctr += 1
+                 
+      
+    return Gen, Glosses
+
+
+def train(T = 1000, Gsave = "E:\ML\Dog-Cat-GANs\Gen_temp.pt"):
+    
+    print('loading data...')
+    dataset = dst.torch_celeb_dataset()
+    print('done.')
+
+    Gen,Gl = diff_train(dataset=dataset,lr = 1E-4, epochs = 100, batch=32, beta1=0.5, beta2=0.999, T=T, dmt = 32, load_state = False)
+   
+    dst.plt.figure(figsize=(10,5))
+    dst.plt.title("Generator Loss During Training")
+    dst.plt.plot(Gl, label='G_loss')
+    dst.plt.legend()
+    dst.plt.xlabel("iterations")
+    dst.plt.ylabel("Loss")
+    dst.plt.legend()
+    dst.plt.show()
+    
+    torch.save(Gen.state_dict(), Gsave)   
+    return Gen
+
+
+def gen_img(Gen = None, T = 1000):
+    with torch.no_grad():
+        t_encode = getPositionEncoding(T,time_encoding_dim = 32)
+        if Gen is None:
+            Gsave = "E:\ML\Dog-Cat-GANs\Gen-diff-Autosave.pt"
+            Gen = models.UNet(3).eval().cuda()
+            try:
+                Gen.load_state_dict(torch.load(Gsave))
+            except:
+                print('Warning: Could not load generator parameters at',Gsave)
+        
+        noise = torch.rand((25,3,140,140)).cuda()
+        for t in range(T):
+            t_en = t_encode[T-t-1].cuda()
+            warped = Gen(noise,t_en)
+            noise = warped
+            
+            if t % 10 == 0:
+                wd = norm(warped).cpu().detach().numpy()
+                dst.visualize_25(wd,dark=False)
+                del wd
+        
+    
+def main():
+    x = int(input('Would you like to train model(press \'1\') or generate synthetic images from previous state(press \'2\')?'))
+    #x=1
+    if x == 1:
+        train()
+    elif x==2:
+        gen_img()
+    else:
+        print('Value Error: Please enter either 1 or 2')
+        
+
+if __name__ == "__main__":
+    main()
