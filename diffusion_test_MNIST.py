4887f24efa5cbd9c46841cfb6d9ce7a14b740599
Agam Chopra <work.agamchopra@gmail.com>
2023-08-24 04:53:12 -0500

    cpu test passed

--- diffusion_test_MNIST.py
+++ diffusion_test_MNIST.py
@@ -10,7 +10,10 @@ import torch
 import torchvision
 import torch.nn as nn
 import matplotlib.pyplot as plt
+import random
 from math import ceil
+from tqdm import trange
+import os
 
 # %% DATASET
 
@@ -26,9 +29,17 @@ def show_images(data, num_samples=9, cols=3):
     plt.show()
 
 
-data = torchvision.datasets.MNIST(
-    root='./data', train=True, download=True, transform=None).data
-data = ((data[:, None] / 255) * 2) - 1
+def fetch_data():
+    data = torchvision.datasets.MNIST(
+        root='./data', train=True, download=True, transform=None).data
+    data = ((data[:, None] / 255) * 2) - 1
+    return data
+
+
+# %% TEST DATASET
+data = fetch_data()
+idx = random.sample(range(0, len(data)), len(data))
+data = data[idx]
 print(data.max(), data.min())
 print(data.shape)
 
@@ -123,6 +134,7 @@ plt.show()
 print(a.shape, b.shape, c.shape)
 
 # %% UNET
+EMBD_DIM = 64
 
 
 class Block(nn.Module):
@@ -156,7 +168,7 @@ class Block(nn.Module):
 
 
 class UNet(nn.Module):
-    def __init__(self, CH=1, t_emb=64, n=1):
+    def __init__(self, CH=1, t_emb=EMBD_DIM, n=1):
         super(UNet, self).__init__()
         # layers
         self.time_mlp = nn.Sequential(nn.Linear(t_emb, t_emb), nn.ReLU())
@@ -182,6 +194,9 @@ class UNet(nn.Module):
             128/n), out_channels=int(128/n), kernel_size=2, stride=2)
 
     def forward(self, x, t):
+        if len(t.shape) < 2:
+            t = t.view(1, EMBD_DIM)
+
         t = self.time_mlp(t)
         y = self.layer1(x)
 
@@ -214,13 +229,14 @@ with torch.no_grad():
 
 
 class Diffusion:
-    def __init__(self, start=0.0001, end=0.01, steps=1000):
+    def __init__(self, start=0.0001, end=0.02, steps=1000):
         self.start = start
         self.end = end
         self.steps = steps
         self.beta = torch.linspace(start, end, steps)
-        self.alpha = 1 - self.betas
-        self.alpha_hat = torch.cumprod(self.alphas, dim=0)
+        self.alpha = 1 - self.beta
+        self.alpha_hat = torch.cumprod(self.alpha, dim=0)
+        self.embeddings = getPositionEncoding(steps, d=EMBD_DIM, n=10000)
 
     def forward(self, x0, t):
         noise = torch.randn_like(x0) - 0.5
@@ -233,13 +249,14 @@ class Diffusion:
 
     @torch.no_grad()
     def backward(self, x, t, model):
+        embeddings_t = self.embeddings[t]
         beta_t = torch.gather(self.beta, dim=-1, index=t).view(-1, 1, 1, 1)
         sqrt_one_minus_alpha_hat_t = torch.gather(torch.sqrt(
             1. - self.alpha_hat), dim=-1, index=t).view(-1, 1, 1, 1)
         sqrt_inv_alpha_t = torch.gather(torch.sqrt(
             1.0 / self.alpha), dim=-1, index=t).view(-1, 1, 1, 1)
         mean = sqrt_inv_alpha_t * \
-            (x - beta_t * model(x, t) / sqrt_one_minus_alpha_hat_t)
+            (x - beta_t * model(x, embeddings_t) / sqrt_one_minus_alpha_hat_t)
         posterior_variance_t = beta_t
 
         if t == 0:
@@ -250,4 +267,125 @@ class Diffusion:
             return mean + varience
 
 
-# %%
+# %% TEST DIFFUSION
+DISP = 10
+diffusion = Diffusion()
+batch = torch.stack([data[0]] * DISP)
+t = torch.linspace(0, diffusion.steps - 1, DISP).long()
+noisy_batch, _ = diffusion.forward(batch, t)
+
+plt.figure(figsize=(5, 5))
+f, ax = plt.subplots(1, DISP, figsize=(100, 100))
+
+for idx, image in enumerate(noisy_batch):
+    ax[idx].imshow(image[0], cmap='gray_r')
+    ax[idx].set_title(f"Iteration: {t[idx].item()}", fontsize=70)
+plt.show()
+
+denoised_batch = [diffusion.backward(noisy_batch[i:i+1],
+                                     t[i:i+1], model)[0] for i in range(DISP)]
+
+plt.figure(figsize=(5, 5))
+f, ax = plt.subplots(1, DISP, figsize=(100, 100))
+
+for idx, image in enumerate(denoised_batch):
+    ax[idx].imshow(image[0], cmap='gray_r')
+    ax[idx].set_title(f"Iteration: {999 - t[idx].item()}", fontsize=70)
+plt.show()
+
+# %% TRAINING LOOP
+
+
+class Dataloader:
+    def __init__(self, batch_size=64):
+        self.data = fetch_data()
+        self.max_id = len(self.data) - 1
+        self.id = 0
+        self.batch = batch_size
+        self.range = range(0, len(self.data))
+        self.randomize()
+
+    def randomize(self):
+        idx = random.sample(self.range, self.max_id + 1)
+        self.data = self.data[idx]
+
+    def sample(self):
+        if self.id + self.batch > self.max_id:
+            if self.id < self.max_id:
+                batch_data = self.data[self.id:]
+            else:
+                batch_data = self.data[self.id:self.id + 1]
+            self.id = 0
+            self.randomize()
+        else:
+            batch_data = self.data[self.id:self.id+self.batch]
+        return batch_data
+
+
+def train(path, epochs=2000, lr=1E-6, batch_size=64, steps=1000, n=1,
+          err_func=nn.L1Loss(), device='cpu'):
+    data = Dataloader(batch_size=batch_size)
+    model = UNet(CH=data.data.shape[1], n=n).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr)
+    train_error = []
+    avg_fact = ceil(len(data.data) / batch_size)
+    tpop = range(1, steps)
+    embds = getPositionEncoding(steps).to(device)
+
+    for eps in range(epochs):
+        print(f'Epoch {eps + 1}|{epochs}')
+        model.train()
+        running_error = 0.
+
+        for _ in trange(0, avg_fact):
+            optimizer.zero_grad()
+            x0 = data.sample().to(device)
+            t = torch.tensor(random.sample(tpop, len(x0)), device=device)
+            xt, noise = forward_sample(x0, t, steps, 0.0001, 0.02)
+            embdt = embds[t]
+
+            pred_noise = model(xt, embdt)
+            error = err_func(noise, pred_noise)
+            error.backward()
+            optimizer.step()
+
+            running_error += error.item()
+
+        train_error.append(running_error / avg_fact)
+        print(f'Average Error: {train_error[-1]}')
+
+        if eps % 10 == 0:
+            plt.figure(figsize=(10, 5))
+            plt.title("Training Noise Prediction Error")
+            plt.plot(train_error, label='Average Error per Epoch')
+            plt.legend()
+            plt.xlabel("Epochs")
+            plt.ylabel("Average L1 Error")
+            plt.legend()
+            plt.show()
+
+            torch.save(model.state_dict(),
+                       os.path.join(path, "diffusion-MNIST-Autosave.pt"))
+
+
+# %% TRAIN
+train(path='/home/ringarty/', epochs=200,
+      lr=1E-3, batch_size=128, steps=500, n=8)
+
+# %% FIN
+diffusion = Diffusion(steps=500)
+
+path = '/home/ringarty/'
+model = UNet(n=8)
+model.load_state_dict(torch.load(os.path.join(path,
+                                              "diffusion-MNIST-Autosave.pt")))
+
+x = torch.randn((1, 1, 28, 28))
+
+for t in range(0, 500):
+    x = diffusion.backward(x, torch.tensor(499 - t), model)
+    if t % 25 == 0 or t == 499:
+        plt.imshow(x[0, 0], cmap='gray_r')
+        plt.axis('off')
+        plt.title(f'Denoising Step: {t}')
+        plt.show()
